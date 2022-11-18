6160871ff175c9e1403a12f0f79d662db0ecd5ba
Jonas Lima Junior <jonasgomeslimajunior@gmail.com>
2022-11-18 19:14:21 -0300

    Final

--- src/main/java/dio/jonas/dioprojectclient/DioProjectClientApplication.java
+++ src/main/java/dio/jonas/dioprojectclient/DioProjectClientApplication.java
@@ -2,7 +2,9 @@ package dio.jonas.dioprojectclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class DioProjectClientApplication {
 
---
+++ src/main/java/dio/jonas/dioprojectclient/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,67 @@
+package dio.jonas.dioprojectclient.service.impl;
+
+import java.util.Optional;
+
+import dio.jonas.dioprojectclient.model.Cliente;
+import dio.jonas.dioprojectclient.model.ClienteRepository;
+import dio.jonas.dioprojectclient.model.Endereco;
+import dio.jonas.dioprojectclient.model.EnderecoRepository;
+import dio.jonas.dioprojectclient.service.ClienteService;
+import dio.jonas.dioprojectclient.service.ViaCepService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private ViaCepService viaCepService;
+
+
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        // Buscar todos os Clientes.
+        return clienteRepository.findAll();
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        return cliente.get();
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+        salvarClienteComCep(cliente);
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+        Optional<Cliente> clienteBd = clienteRepository.findById(id);
+        if (clienteBd.isPresent()) {
+            salvarClienteComCep(cliente);
+        }
+    }
+
+    @Override
+    public void deletar(Long id) {
+        // Deletar Cliente por ID.
+        clienteRepository.deleteById(id);
+    }
+
+    private void salvarClienteComCep(Cliente cliente) {
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+            Endereco novoEndereco = viaCepService.consultarCep(cep);
+            enderecoRepository.save(novoEndereco);
+            return novoEndereco;
+        });
+        cliente.setEndereco(endereco);
+        clienteRepository.save(cliente);
+    }
+
+}
